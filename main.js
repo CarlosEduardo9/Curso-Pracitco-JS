5ed2bf759a837a47dd25a33fb6f67cb2d2a72741
Carlos <carlostabaresduran1@gmail.com>
2023-10-16 18:59:40 -0300

    se agrego menu mobile y el carrito

--- main.js
+++ main.js
@@ -2,19 +2,26 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const MenuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside= document.querySelector('.product-detail');
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 MenuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
-function toggleDesktopMenu() {
-  
+function toggleDesktopMenu() { 
   desktopMenu.classList.toggle('inactive');
+
 }
 function toggleMobileMenu() {
-  
+  aside.classList.add('inactive');
   mobileMenu.classList.toggle('inactive');
 }
-
+function toggleCarritoAside() {
+  
+ mobileMenu.classList.add('inactive');
+ aside.classList.toggle('inactive');
+}
 
 
