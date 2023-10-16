e3db0b70d21f36d4a30695a034074d21de528c60
Carlos <carlostabaresduran1@gmail.com>
2023-10-16 15:14:15 -0300

    agregado el menu mobile

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const MenuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+MenuHamIcon.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
-  console.log('Click');
+  
   desktopMenu.classList.toggle('inactive');
 }
+function toggleMobileMenu() {
+  
+  mobileMenu.classList.toggle('inactive');
+}
 
-console.log('JS funcionando');
+
+
